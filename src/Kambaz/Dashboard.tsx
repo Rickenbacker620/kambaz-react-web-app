ddd8999c666bae49326e32040c8337ff4bb0a330
Rickenbacker620 <fu78sion@gmail.com>
2025-02-05 17:35:58 -0500

    finished home screen

--- src/Kambaz/Dashboard.tsx
+++ src/Kambaz/Dashboard.tsx
@@ -1,115 +1,182 @@
 import { Link } from "react-router-dom";
+import { Row, Col, Card, Button } from "react-bootstrap";
+
 export default function Dashboard() {
   return (
     <div id="wd-dashboard">
       <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
       <h2 id="wd-dashboard-published">Published Courses (12)</h2> <hr />
       <div id="wd-dashboard-courses">
-        <div className="wd-dashboard-course">
-          <Link
-            to="/Kambaz/Courses/1234/Home"
-            className="wd-dashboard-course-link"
-          >
-            <img src="/images/reactjs.jpg" width={200} />
-            <div>
-              <h5> CS1234 React JS </h5>
-              <p className="wd-dashboard-course-title">
-                Full Stack software developer
-              </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link
-            to="/Kambaz/Courses/5678/Home"
-            className="wd-dashboard-course-link"
-          >
-            <img src="/images/reactjs.jpg" width={200} />
-            <div>
-              <h5> CS5678 Advanced React </h5>
-              <p className="wd-dashboard-course-title">
-                Mastering React for Professionals
-              </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link
-            to="/Kambaz/Courses/9101/Home"
-            className="wd-dashboard-course-link"
-          >
-            <img src="/images/reactjs.jpg" width={200} />
-            <div>
-              <h5> CS9101 Node.js Essentials </h5>
-              <p className="wd-dashboard-course-title">
-                Backend Development with Node.js
-              </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link
-            to="/Kambaz/Courses/1121/Home"
-            className="wd-dashboard-course-link"
-          >
-            <img src="/images/reactjs.jpg" width={200} />
-            <div>
-              <h5> CS1121 Express.js Basics </h5>
-              <p className="wd-dashboard-course-title">
-                Building Web Applications with Express.js
-              </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link
-            to="/Kambaz/Courses/3141/Home"
-            className="wd-dashboard-course-link"
-          >
-            <img src="/images/reactjs.jpg" width={200} />
-            <div>
-              <h5> CS3141 MongoDB for Developers </h5>
-              <p className="wd-dashboard-course-title">
-                Database Management with MongoDB
-              </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link
-            to="/Kambaz/Courses/5161/Home"
-            className="wd-dashboard-course-link"
-          >
-            <img src="/images/reactjs.jpg" width={200} />
-            <div>
-              <h5> CS5161 Full Stack Development </h5>
-              <p className="wd-dashboard-course-title">
-                Comprehensive Guide to Full Stack Development
-              </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link
-            to="/Kambaz/Courses/7181/Home"
-            className="wd-dashboard-course-link"
-          >
-            <img src="/images/reactjs.jpg" width={200} />
-            <div>
-              <h5> CS7181 TypeScript Deep Dive </h5>
-              <p className="wd-dashboard-course-title">
-                Advanced TypeScript Programming
-              </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
+        <Row xs={1} md={5} className="g-4">
+          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+            <Card>
+              <Link
+                to="/Kambaz/Courses/1234/Home"
+                className="wd-dashboard-course-link text-decoration-none text-dark"
+              >
+                <Card.Img
+                  variant="top"
+                  src="/images/reactjs.jpg"
+                  width="100%"
+                  height={160}
+                />
+                <Card.Body>
+                  <Card.Title className="wd-dashboard-course-title">
+                    CS1234 React JS
+                  </Card.Title>
+                  <Card.Text className="wd-dashboard-course-description">
+                    Full Stack software developer
+                  </Card.Text>
+                  <Button variant="primary">Go</Button>
+                </Card.Body>
+              </Link>
+            </Card>
+          </Col>
+          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+            <Card>
+              <Link
+                to="/Kambaz/Courses/5678/Home"
+                className="wd-dashboard-course-link text-decoration-none text-dark"
+              >
+                <Card.Img
+                  variant="top"
+                  src="/images/reactjs.jpg"
+                  width="100%"
+                  height={160}
+                />
+                <Card.Body>
+                  <Card.Title className="wd-dashboard-course-title">
+                    CS5678 Advanced React
+                  </Card.Title>
+                  <Card.Text className="wd-dashboard-course-description">
+                    Mastering React for Professionals
+                  </Card.Text>
+                  <Button variant="primary">Go</Button>
+                </Card.Body>
+              </Link>
+            </Card>
+          </Col>
+          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+            <Card>
+              <Link
+                to="/Kambaz/Courses/9101/Home"
+                className="wd-dashboard-course-link text-decoration-none text-dark"
+              >
+                <Card.Img
+                  variant="top"
+                  src="/images/reactjs.jpg"
+                  width="100%"
+                  height={160}
+                />
+                <Card.Body>
+                  <Card.Title className="wd-dashboard-course-title">
+                    CS9101 Node.js Essentials
+                  </Card.Title>
+                  <Card.Text className="wd-dashboard-course-description">
+                    Backend Development with Node.js
+                  </Card.Text>
+                  <Button variant="primary">Go</Button>
+                </Card.Body>
+              </Link>
+            </Card>
+          </Col>
+          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+            <Card>
+              <Link
+                to="/Kambaz/Courses/1121/Home"
+                className="wd-dashboard-course-link text-decoration-none text-dark"
+              >
+                <Card.Img
+                  variant="top"
+                  src="/images/reactjs.jpg"
+                  width="100%"
+                  height={160}
+                />
+                <Card.Body>
+                  <Card.Title className="wd-dashboard-course-title">
+                    CS1121 Express.js Basics
+                  </Card.Title>
+                  <Card.Text className="wd-dashboard-course-description">
+                    Building Web Applications with Express.js
+                  </Card.Text>
+                  <Button variant="primary">Go</Button>
+                </Card.Body>
+              </Link>
+            </Card>
+          </Col>
+          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+            <Card>
+              <Link
+                to="/Kambaz/Courses/3141/Home"
+                className="wd-dashboard-course-link text-decoration-none text-dark"
+              >
+                <Card.Img
+                  variant="top"
+                  src="/images/reactjs.jpg"
+                  width="100%"
+                  height={160}
+                />
+                <Card.Body>
+                  <Card.Title className="wd-dashboard-course-title">
+                    CS3141 MongoDB for Developers
+                  </Card.Title>
+                  <Card.Text className="wd-dashboard-course-description">
+                    Database Management with MongoDB
+                  </Card.Text>
+                  <Button variant="primary">Go</Button>
+                </Card.Body>
+              </Link>
+            </Card>
+          </Col>
+          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+            <Card>
+              <Link
+                to="/Kambaz/Courses/5161/Home"
+                className="wd-dashboard-course-link text-decoration-none text-dark"
+              >
+                <Card.Img
+                  variant="top"
+                  src="/images/reactjs.jpg"
+                  width="100%"
+                  height={160}
+                />
+                <Card.Body>
+                  <Card.Title className="wd-dashboard-course-title">
+                    CS5161 Full Stack Development
+                  </Card.Title>
+                  <Card.Text className="wd-dashboard-course-description">
+                    Comprehensive Guide to Full Stack Development
+                  </Card.Text>
+                  <Button variant="primary">Go</Button>
+                </Card.Body>
+              </Link>
+            </Card>
+          </Col>
+          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+            <Card>
+              <Link
+                to="/Kambaz/Courses/7181/Home"
+                className="wd-dashboard-course-link text-decoration-none text-dark"
+              >
+                <Card.Img
+                  variant="top"
+                  src="/images/reactjs.jpg"
+                  width="100%"
+                  height={160}
+                />
+                <Card.Body>
+                  <Card.Title className="wd-dashboard-course-title">
+                    CS7181 TypeScript Deep Dive
+                  </Card.Title>
+                  <Card.Text className="wd-dashboard-course-description">
+                    Advanced TypeScript Programming
+                  </Card.Text>
+                  <Button variant="primary">Go</Button>
+                </Card.Body>
+              </Link>
+            </Card>
+          </Col>
+        </Row>
       </div>
     </div>
   );
