794f1b8f496a3730c46d3083f969e8834086eaea
Rickenbacker620 <fu78sion@gmail.com>
2025-03-11 13:00:38 -0400

    1

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -1,43 +1,47 @@
+import { Nav } from "react-bootstrap";
 import { useLocation } from "react-router";
+import NavItem from "react-bootstrap/NavItem";
+import NavLink from "react-bootstrap/NavLink";
+
 export default function TOC() {
   const { pathname } = useLocation();
   return (
-    <ul className="nav nav-pills" id="wd-toc">
-      <li className="nav-item">
-        <a id="wd-a" href="#/Labs" className="nav-link">
+    <Nav variant="pills" id="wd-toc">
+      <NavItem>
+        <NavLink id="wd-a" href="#/Labs">
           Labs
-        </a>
-      </li>
-      <li className="nav-item">
-        <a id="wd-a1" href="#/Labs/Lab1" className={`nav-link ${pathname.includes("Lab1") ? "active" : ""}`}>
+        </NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink id="wd-a1" href="#/Labs/Lab1" className={pathname.includes("Lab1") ? "active" : ""}>
           Lab 1
-        </a>
-      </li>
-      <li className="nav-item">
-        <a id="wd-a2" href="#/Labs/Lab2" className={`nav-link ${pathname.includes("Lab2") ? "active" : ""}`}>
+        </NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink id="wd-a2" href="#/Labs/Lab2" className={pathname.includes("Lab2") ? "active" : ""}>
           Lab 2
-        </a>
-      </li>
-      <li className="nav-item">
-        <a id="wd-a3" href="#/Labs/Lab3" className={`nav-link ${pathname.includes("Lab3") ? "active" : ""}`}>
+        </NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink id="wd-a3" href="#/Labs/Lab3" className={pathname.includes("Lab3") ? "active" : ""}>
           Lab 3
-        </a>
-      </li>
-      <li className="nav-item">
-        <a id="wd-a3" href="#/Labs/Lab4" className={`nav-link ${pathname.includes("Lab4") ? "active" : ""}`}>
+        </NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink id="wd-a3" href="#/Labs/Lab4" className={pathname.includes("Lab4") ? "active" : ""}>
           Lab 4
-        </a>
-      </li>
-      <li className="nav-item">
-        <a id="wd-k" href="#/Kambaz" className="nav-link">
+        </NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink id="wd-k" href="#/Kambaz">
           Kambaz
-        </a>
-      </li>
-      <li className="nav-item">
-        <a id="wd-github" href="https://github.com/jannunzi" target="_blank" className="nav-link">
+        </NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink id="wd-github" href="https://github.com/jannunzi" target="_blank">
           My GitHub
-        </a>
-      </li>
-    </ul>
+        </NavLink>
+      </NavItem>
+    </Nav>
   );
 }
